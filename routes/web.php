7c47d274c1d898be9f59d8beffdaa276d31e3565
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2023-01-12 17:08:06 +0100

    added model with controller, seeder and migration - close #4

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ItemController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
 
 Route::middleware('auth', 'verified')
@@ -25,6 +26,7 @@ Route::middleware('auth', 'verified')
     ->prefix('admin')
     ->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('items', ItemController::class);
     });
 
 require __DIR__ . '/auth.php';
