60c970a89eb6d471500bd93da5c864a16abe6e31
paoloevan <paoloev1900@gmail.com>
2023-01-18 12:43:44 +0100

    fix bug

--- resources/views/admin/items/show.blade.php
+++ resources/views/admin/items/show.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <img width="200" src="{{ asset('storage/' . $item->cover_image) }}" alt="{{ $item->name }}">
     <h1>{{ $item->id }}</h1>
-    <h1>{{ $item->name }}</h1>
+    <h1>{{ $item->title }}</h1>
     <h1>{{ $item->body }}</h1>
 @endsection
